1d7d0b592e49a672fe909060244af4fa2678555d
Kellie Knight <kellieeknight@gmail.com>
2017-10-11 16:59:44 -0700

    Enemy formation movement

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour {
 
     public GameObject enemyPrefab;
+    public float width = 10f;
+    public float height = 5f;
+    public float speed;
+
+    private bool movingRight;
+    private float xmax;
+    private float xmin;
 
 	// Use this for initialization
 	void Start () {
 
-        
+        movingRight = false;
+
+
+        //Calculate edge of screen
+        float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanceToCamera));
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distanceToCamera));
+        xmax = rightEdge.x;
+        xmin = leftEdge.x;
+
+
         /*Loop over every child object
          grab their transform
          spawn an enemy on top of every position*/
-        foreach(Transform child in transform)
+        foreach (Transform child in transform)
         {
             //Instantiate enemies in the same place of their parent object (position)
             GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
@@ -23,9 +40,36 @@ public class EnemySpawner : MonoBehaviour {
         }
 
 	}
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        float formationRightEdge = transform.position.x + (width / 2);
+        float formationLeftEdge = transform.position.x - (width / 2);
+
+        if (formationRightEdge > xmax)
+        {
+            movingRight = false;
+
+        }else if(formationLeftEdge < xmin)
+        {
+            movingRight = true;
+        }
+
+        if (movingRight)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        else
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+
 	}
 }
